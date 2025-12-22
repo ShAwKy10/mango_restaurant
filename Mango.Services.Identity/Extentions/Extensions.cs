6ee843e845221991b306f60af6308cbad8cca300
Shawky <amamasa121212@gmail.com>
2025-12-22 22:26:47 +0200

    Add initial project structure and files

---
+++ Mango.Services.Identity/Extentions/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace Mango.Services.Identity
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Checks if the authentication scheme supports signout.
+        /// </summary>
+        public static async Task<bool> GetSchemeSupportsSignOutAsync(this HttpContext context, string scheme)
+        {
+            var provider = context.RequestServices.GetRequiredService<IAuthenticationHandlerProvider>();
+            var handler = await provider.GetHandlerAsync(context, scheme);
+            return (handler is IAuthenticationSignOutHandler);
+        }
+    }
+}
